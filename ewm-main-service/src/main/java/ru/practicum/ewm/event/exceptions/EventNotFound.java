2334335a5745ba268e39f001b8cee8b249203013
Aleksandr <aliaksandrmo@yandex.ru>
2025-09-18 23:28:19 +0300

    fix: apply exception model changes

--- ewm-main-service/src/main/java/ru/practicum/ewm/event/exceptions/EventNotFound.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/event/exceptions/EventNotFound.java
@@ -2,10 +2,8 @@ package ru.practicum.ewm.event.exceptions;
 
 import ru.practicum.ewm.exception.model.NotFoundException;
 
-import java.time.LocalDateTime;
-
 public class EventNotFound extends NotFoundException {
-    public EventNotFound(String message, String reason, LocalDateTime timestamp) {
-        super(message, reason, timestamp);
+    public EventNotFound(String message, String reason) {
+        super(message, reason);
     }
 }
