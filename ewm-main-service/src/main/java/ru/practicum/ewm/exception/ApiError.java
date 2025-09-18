26f7ff040e8709c462c25c3acc354ecadd6bb905
Aleksandr <aliaksandrmo@yandex.ru>
2025-09-18 22:49:08 +0300

    fix: exception model

--- ewm-main-service/src/main/java/ru/practicum/ewm/exception/ApiError.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/exception/ApiError.java
@@ -1,20 +1,19 @@
 package ru.practicum.ewm.exception;
 
 import ru.practicum.ewm.exception.model.EwmException;
-import ru.practicum.ewm.exception.model.ExceptionStatus;
 
 import java.time.LocalDateTime;
 
 public class ApiError {
+    private final String status;
     private final String message;
     private final String reason;
-    private final ExceptionStatus status;
     private final LocalDateTime timestamp;
 
     public ApiError(EwmException exception){
         this.message = exception.getMessage();
         this.reason = exception.getReason();
-        this.status = exception.getStatus();
+        this.status = exception.getStatus().toString();
         this.timestamp = exception.getTimestamp();
     }
 }
--- ewm-main-service/src/main/java/ru/practicum/ewm/exception/model/ExceptionStatus.java
+++
@@ -1,5 +0,0 @@
-package ru.practicum.ewm.exception.model;
-
-public enum ExceptionStatus {
-    BAD_REQUEST, NOT_FOUND, CONFLICT, FORBIDDEN;
-}
