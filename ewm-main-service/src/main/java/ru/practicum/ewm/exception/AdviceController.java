b7b2690204af4f2f029247e896209edbb4e7aa08
Aleksandr <aliaksandrmo@yandex.ru>
2025-09-18 22:50:35 +0300

    fix: advice controller

--- ewm-main-service/src/main/java/ru/practicum/ewm/exception/AdviceController.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/exception/AdviceController.java
@@ -7,26 +7,24 @@ import ru.practicum.ewm.exception.model.DataViolationException;
 import ru.practicum.ewm.exception.model.NotExistsException;
 import ru.practicum.ewm.exception.model.NotFoundException;
 
-import static org.springframework.http.HttpStatus.*;
-
 @RestControllerAdvice
 public class AdviceController {
 
     //Response Code is 404
     @ExceptionHandler(NotFoundException.class)
     ResponseEntity<ApiError> handleNotFoundException(NotFoundException exception) {
-        return new ResponseEntity<>(new ApiError(exception), NOT_FOUND);
+        return new ResponseEntity<>(new ApiError(exception), exception.getStatus());
     }
 
     //Response Code is 409
     @ExceptionHandler(NotExistsException.class)
     ResponseEntity<ApiError> handleNotExistsException(NotExistsException exception) {
-        return new ResponseEntity<>(new ApiError(exception), CONFLICT);
+        return new ResponseEntity<>(new ApiError(exception), exception.getStatus());
     }
 
     //Response Code is 400
     @ExceptionHandler(DataViolationException.class)
     ResponseEntity<ApiError> handleDataViolationException(DataViolationException exception) {
-        return new ResponseEntity<>(new ApiError(exception), BAD_REQUEST);
+        return new ResponseEntity<>(new ApiError(exception), exception.getStatus());
     }
 }
